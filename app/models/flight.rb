91ffa025faedb2154c4316ea510756e8d4548e41
Scott Robel Jr <scott.robel.jr@gmail.com>
2020-05-07 17:25:18 -0400

    create associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,4 @@
 class Flight < ApplicationRecord
+  belongs_to :from_airport, class_name: 'Airport', foreign_key: :start_airport_id
+  belongs_to :to_airport, class_name: 'Airport', foreign_key: :end_airport_id
 end
